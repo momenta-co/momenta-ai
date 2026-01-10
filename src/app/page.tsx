4bd88740cfa6951a8807ebc0ef58b00c8d2167b5
Nathaly <natharb09@gmail.com>
2026-01-09 22:07:30 -0500

    feat: hiding the additional sections for the beta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,10 @@
 import { Hero } from "@/components/sections/Hero";
 import { Footer } from "@/components/layout/Footer";
-import { Browse } from "@/components/sections/Browse";
-import { Feed } from "@/components/sections/Feed";
-import { getFeedExperiences } from "@/lib/db/feed";
 
 export default async function HomePage() {
-  // Fetch experiences from database
-  const feedData = await getFeedExperiences();
-
   return (
     <>
       <Hero />
-      <Browse />
-      <Feed
-        tonightIdeas={feedData.tonightIdeas}
-        stressRelief={feedData.stressRelief}
-      />
       <Footer />
     </>
   );
